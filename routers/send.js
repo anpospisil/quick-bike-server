6e1bcafe8349137f3485f6e49e9815d6a5ccd2be
anpospisil <anpospisil89@gmail.com>
2020-07-30 17:48:18 +0200

    added endpoint for reservation ended post request

--- routers/send.js
+++ routers/send.js
@@ -7,7 +7,6 @@ router.post('/', authMiddleware, async (req, res, next) => {
     try{
         const { name } = req.body
         const user = req.user;
-        console.log("NAME", name)
         const transporter = nodemailer.createTransport({
             host: 'smtp.ethereal.email',
             port: 587,
@@ -35,5 +34,36 @@ router.post('/', authMiddleware, async (req, res, next) => {
         next(e);
       }
     });
+
+    router.post('/end', authMiddleware, async (req, res, next) => {
+      try{
+          const user = req.user;
+          const transporter = nodemailer.createTransport({
+              host: 'smtp.ethereal.email',
+              port: 587,
+              auth: {
+                  user: 'waylon.yost@ethereal.email',
+                  pass: 'eU6F6GhwSZ83mTx1Sx'
+              }
+          });
+          const mailOptions = {
+              from: `${req.body.email}`,
+              to: user.email,
+              subject: `Reservation Ended - Quick Bike`,
+              text: `Hey there, ${user.name}! Your reservation has ended. Thank you for using Quick Bike!`,
+              replyTo: `waylon.yost@ethereal.email`
+            }
+            transporter.sendMail(mailOptions, function(err, res) {
+              if (err) {
+                console.error('there was an error: ', err);
+              } else {
+                console.log('here is the res: ', res)
+              }
+          })
+          return res.status(200).send({ message: "confirmation email sent!" });
+      } catch (e) { 
+          next(e);
+        }
+      });
 
 module.exports = router;
